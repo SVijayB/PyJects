e47e7bd11589eb6c0b979400d964b8102c20a114
vijay <vijaybalajitheboss@gmail.com>
2020-07-08 15:47:23 +0530

    Created a seperate function for application password

--- Password Manager/src/Password_manager.py
+++ Password Manager/src/Password_manager.py
@@ -15,31 +15,7 @@ if __name__ == "__main__":
     except:
         pass
 
-    try:
-        file = open('../temp/password.txt', 'r')
-        password = file.read()
-        while(True):
-            entered_password = input("Enter your password \n> ")
-            if(password == entered_password):
-                green("Logged in successfully")
-                grey("Press any key to continue...")
-                input()
-                break
-            else:
-                red("Incorrect password")
-    except:
-        green("PASSWORD MANAGER")
-        cyan("-" * 25)
-        print("Since, this is the first time you are using Password_manager, Create a password.")
-        red("DO NOT FORGET THIS PASSWORD. YOU CANNOT CHANGE IT LATER.")
-        password = input("Enter your password \n> ")
-        file = open('../temp/password.txt', 'w')
-        file.write(password)
-        green("Your password has been saved successfully")
-        cyan("Relaunch the application to continue.")
-        grey("Press enter to exit...")
-        input()
-        sys.exit(0)
+    password_function()
 
     conn = sqlite3.connect('../assets/Database.db')
     cursor = conn.cursor()
